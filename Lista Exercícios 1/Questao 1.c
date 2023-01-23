314554cec836b0346adb94e088395e6737880555
mikaelsfm <mikael.mesquita@gmail.com>
2023-01-22 21:24:34 -0300

    lista 1

--- Lista Exercícios 1/Questao 1.c
+++ Lista Exercícios 1/Questao 1.c
@@ -2,7 +2,6 @@
 //e determine o comprimento máximo de umsegmento crescente destes n (números. Exemplos:Na sequência 5, 10, 3,2, 4, 7, 9, 8, 5 o comprimento do
 //segmento crescente máximo é 4.Na sequência 10, 8, 7, 5, 2 o comprimento de um segmento crescente máximo é 1.Seu programa para quando n for menor ou igual a zero.
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
